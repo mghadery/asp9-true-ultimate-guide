3607a67f8464affe42aa72fede21d05a816cae6f
ghaderi <mghadery@gmail.com>
2025-01-28 22:55:54 +0100

    Section11

---
+++ Section11.Practice/ViewComponents/GridViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Section11.Practice.ViewComponents
+{
+    public class GridViewComponent:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string title, int x)
+        {
+            ViewBag.Title = title + " " + x;
+            return View();
+        }
+    }
+}
