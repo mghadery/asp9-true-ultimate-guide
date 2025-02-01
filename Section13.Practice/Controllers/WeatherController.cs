614cbaddac8f89e60691e042cacbf1305e4b42c1
ghaderi <mghadery@gmail.com>
2025-02-01 19:29:58 +0100

    Section13

---
+++ Section13.Practice/Controllers/WeatherController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Section13.Practice.Interfaces;
+
+namespace Section13.Practice.Controllers
+{
+    public class WeatherController(IWeatherService weatherService, IWebHostEnvironment webHostEnvironment) : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            var weatherList = weatherService.GetWeatherDetails();
+            return View(weatherList);
+        }
+        [Route("/weather/{cityCode}")]
+        public IActionResult GetCityWeather(string cityCode)
+        {
+            var r = weatherService.GetWeatherByCityCode(cityCode);
+            if (r is null)
+                return View("_Error");
+            return View(r);
+        }
+        [Route("/error")]
+        public IActionResult Error()
+        {
+            throw new Exception("test exception handling");
+        }
+        [Route("/env")]
+        public IActionResult Env()
+        {
+            return Content(webHostEnvironment.EnvironmentName);
+        }
+    }
+}
---
+++ Section13.Practice/Interfaces/IWeatherService.cs
@@ -0,0 +1,9 @@
+﻿using Section13.Practice.Models;
+
+namespace Section13.Practice.Interfaces;
+
+public interface IWeatherService
+{
+    CityWeather? GetWeatherByCityCode(string cityCode);
+    List<CityWeather> GetWeatherDetails();
+}
