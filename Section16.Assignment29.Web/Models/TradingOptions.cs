75fa6dc5cb3aacad17503a978d5e92809fe0958e
ghaderi <mghadery@gmail.com>
2025-02-14 18:16:17 +0100

    Section16.Assignment29

---
+++ Section16.Assignment29.Web/Models/TradingOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace Section16.Assignment29.Models;
+
+public class TradingOptions
+{
+    public string? DefaultStockSymbol { get; set; }
+    public uint DefaultOrderQuantity { get; set; }
+}
---
+++ Section16.Assignment29.Web/Program.cs
@@ -0,0 +1,17 @@
+using Section16.Assignment29.Models;
+using Section16.Assignment29.ServiceContracts.Interfaces;
+using Section16.Assignment29.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IFinnhubService, FinnhubService>();
+builder.Services.AddSingleton<IStocksService, StocksService>();
+builder.Services.AddHttpClient();
+builder.Services.Configure<TradingOptions>(builder.Configuration.GetSection("TradingOptions"));
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
