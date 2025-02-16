db42aaed57f0eb4f24498b7f605101919fa1bd36
ghaderi <mghadery@gmail.com>
2025-02-16 12:37:13 +0100

    Section17.Assignment31

---
+++ Section17.Assignment31/Models/Orders.cs
@@ -0,0 +1,9 @@
+﻿using Section16.Assignment29.ServiceContracts.DTOs;
+
+namespace Section17.Assignment31.Web.Models;
+
+public class Orders
+{
+    public List<BuyOrderResponse> BuyOrders { get; set; }
+    public List<SellOrderResponse> SellOrders { get; set; }
+}
